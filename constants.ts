1bb86e5965c47ca85b902fea09c254e24280cba3
Dumisani Mathabathe <dumimatha3@gmail.com>
2025-04-24 22:55:14 +0200

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,40 @@
+import multiBluetoothImage from "../assets/multibluetooth.jpg";
+
+export const CONTACT_INFO = {
+  name: "Dumisani Mathabathe",
+  phone: "081485513",
+  email: "dumimatha3@gmail.com"
+};
+
+export const SOCIAL_LINKS = {
+  github: "https://github.com/KarlDM3",
+  linkedin: "https://linkedin.com/in/dumisani-mathabathe",
+  twitter: "https://x.com/Dumi_ZA3",
+  instagram: "https://www.instagram.com/dumisani_mthi/"
+};
+
+export const SKILLS = [
+  { name: "Penetration Testing", percentage: 90 },
+  { name: "Network Security", percentage: 85 },
+  { name: "Python", percentage: 95 },
+  { name: "Web Development", percentage: 80 },
+  { name: "Cryptography", percentage: 75 },
+  { name: "System Administration", percentage: 85 }
+];
+
+export const CERTIFICATIONS = [
+  "CS50x Certificate from Harvard Online",
+  "Cisco Networking Academy Cybersecurity Certificate",
+  "SoloLearn Python Certificate",
+  "SoloLearn Java Certificate"
+];
+
+export const PROJECTS = [
+  {
+    title: "MultiBluetooth Speaker System",
+    description: "A system that enables synchronized audio playback across multiple Bluetooth speakers, creating an immersive sound experience.",
+    technologies: ["Bluetooth", "Python", "Audio Processing"],
+    image: multiBluetoothImage,
+    github: "https://github.com/karlDM3/MultiBluetooth-Speaker-SYSTEM"
+  }
+];
